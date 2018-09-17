be9af67fcb683cb75b1f08ec912048f7bebd3fc0
Bryce McDonnell <bryce@bridgetownint.com>
2018-09-17 13:22:02 -0400

    Only whitelist env vars

--- lib/common.sh
+++ lib/common.sh
@@ -5,11 +5,14 @@ head() {
 
 export_env_dir() {
   env_dir=$1
-  whitelist_regex=${2:-'^(NODE_ENV)$'}
+  whitelist_regex=${2:-'^(NODE_ENV|AWS_REGION|GOOGLE_ANALYTICS_KEY|MIXPANEL_KEY|VOTE_REDIRECT)$'}
   blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
   if [ -d "$env_dir" ]; then
     for e in $(ls $env_dir); do
-      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      # this was the original line
+      # echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      # Modifying this so env vars are imported into the app
+      echo "$e" | grep -E "$whitelist_regex" &&
       export "$e=$(cat $env_dir/$e)"
       :
     done
